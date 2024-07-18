54ce72f2ef0a6ccc429fc50e249dcb7231e132ad
samruddhi kulkarni <130344582+samruddhi22022001@users.noreply.github.com>
2024-07-18 11:32:17 +0000

    variable and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 121212
+let accountEmail = "samruddhi@22gmail.com"
+var accountPassword = "111111"
+accountCity = "Nagpur"
+let accoutState;
+
+
+console.table([accountId, accountPassword, accountEmail, accountCity, accoutState]);
+
+// avoid using var because of scope issues 
+//const can not be changed
+
+
+//number
+//bigint
+//bollean
+//string
+//null
+//object
+//undefine
