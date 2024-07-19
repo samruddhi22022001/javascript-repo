ed52d5579d6266396de11c6f51c27d4ff5d3ec1e
samruddhi kulkarni <130344582+samruddhi22022001@users.noreply.github.com>
2024-07-19 09:35:59 +0000

    operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -32,3 +32,32 @@ console.log(booleanIsLoggedIn)
 let baba = 33
 let StringBaba = String(baba);
 console.log(typeof StringBaba);
+
+//**************************operations***************************//
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+str1 = "hello";
+str2 = " samruddhi";
+str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log("2" + 1+ 2);
+// console.log(1 + 2 + "3");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
