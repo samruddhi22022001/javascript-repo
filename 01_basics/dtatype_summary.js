ee70b80036c334fe5cadb1e574b8835e60f31be4
samruddhi kulkarni <130344582+samruddhi22022001@users.noreply.github.com>
2024-07-19 10:13:04 +0000

    datatype summary

---
+++ 01_basics/dtatype_summary.js
@@ -0,0 +1,44 @@
+//premitive datatype  (call by value )//
+
+//7 types : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id=== anotherId);
+
+const bigNumber = 325546n
+
+
+
+//JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+
+//Nonpremitive (refference type)//
+
+//Array, objects, Functions
+
+//Array
+const heros = ["shaktiman", "naagraj", "daga"];
+
+// object
+let myObj = {
+    name:"hitesh",
+    age: 22,
+
+}
+
+//function 
+
+const myFunction = function (){
+
+    console.log("Hello world")
+
+}
