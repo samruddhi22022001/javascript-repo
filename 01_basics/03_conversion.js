ca34f87c45bba3e62366fd49d529ee38588f0144
samruddhi kulkarni <130344582+samruddhi22022001@users.noreply.github.com>
2024-07-19 09:13:18 +0000

    conversion of data

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,34 @@
+let score = 33;
+let marks = "33";
+
+console.log(typeof score);
+console.log(typeof marks);
+
+let numbertostring = String(score);
+let stringToNumber = Number(score);
+
+console.log(typeof numbertostring);
+console.log(typeof stringToNumber);
+
+console.log(numbertostring);
+
+// "33" to 33
+//"33abc" to NaN
+//true to 1 ; false to 0
+// null to zero 
+//undefined to NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn)
+
+//1 to true ; 0 to false
+//"" ti false 
+//"samruddhi" to true
+
+let baba = 33
+let StringBaba = String(baba);
+console.log(typeof StringBaba);
