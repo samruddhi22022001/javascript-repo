2e4b39fd301f2f0fb0dfa7ddb3d86bcfac5f64fb
samruddhi kulkarni <130344582+samruddhi22022001@users.noreply.github.com>
2024-07-19 09:49:31 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+//* Comparision of datatypes in javascript */
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+
+//different datatatypes comparision
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>= 0); //it converts null to 0 and then compare 
+
+//should avoid avoid comparision
+
+// === it will check its datatype also 
+
+console.log("2" === 2);
+
