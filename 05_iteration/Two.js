bfa559bdba595f825a17c2362edf04d1d0027dff
samruddhi kulkarni <130344582+samruddhi22022001@users.noreply.github.com>
2024-07-23 14:16:41 +0000

    while do while loop

---
+++ 05_iteration/Two.js
@@ -0,0 +1,33 @@
+//ehile loop
+
+let index = 0;
+while (index<=20) {
+    console.log(`value of index is ${index}`);
+    index = index +2;
+    
+}
+
+let myarr = [56, 65, 99, 89, 46]
+let arr = 0;
+
+while (arr<myarr.length) {
+    console.log(`value is ${myarr[arr]}`);
+    arr = arr +1;
+    
+}
+
+
+//do while 
+
+let score = 0;
+
+do {
+    console.log(score);
+    score++
+} while (score<=10);
+
+let num = 11
+do {
+    console.log(num);
+    num++
+} while (num<=10);
