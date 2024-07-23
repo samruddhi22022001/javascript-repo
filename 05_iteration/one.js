2d9fbdfb0f61ab10f63ee9e954ea55bf62afddbf
samruddhi kulkarni <130344582+samruddhi22022001@users.noreply.github.com>
2024-07-23 14:03:43 +0000

    iteration for loop brek and continue

---
+++ 05_iteration/one.js
@@ -0,0 +1,56 @@
+//for loop
+
+for(let i = 0; i<=10; i++){
+    console.log(i);
+    if(i==5){
+        console.log(" 5 is the best number. ");
+    }
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`outer loop ${i}`);
+   for (let j = 0; j <=10; j++) {
+    console.log(`Inner loop ${j}  and inner loop value ${i}`);
+    
+   }
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Table for ${i}`);
+   for (let j = 1; j <=10; j++) {
+    console.log(i + " * " + j + " = " + i*j);
+    
+   }
+    
+}
+
+//loop on array
+ 
+let myarr = [25, 56, 68, 89]
+
+for (let i = 0; i <myarr.length; i++) {
+    
+    console.log(myarr[i]);
+}
+
+
+// break and continue
+
+
+for (let index = 1; index <=20; index++) {
+    if(index==5){
+        console.log("detected 5 ");
+        break;
+    }
+    console.log(`value og i is ${index}`);
+    
+}
+for (let index = 1; index <=20; index++) {
+    if(index==5){
+        console.log("detected 5 ");
+        continue;
+    }
+    console.log(`value of i is ${index}`);
+    
+}
